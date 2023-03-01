a66b011fa8e30cf46ba6d28d57d159a88032287e
SIDI Mohamed YAhdhih <34769155+mosidi@users.noreply.github.com>
2023-03-01 05:20:26 +0100

    Update download.py

--- download.py
+++ download.py
@@ -2,10 +2,11 @@
 # It runs during container build time to get model weights built into the container
 import os
 import wget 
-URL = "https://huggingface.co/spaces/mosidi/fi-ber-detec-api/raw/main/model_final.pth"
+URL = "https://huggingface.co/spaces/mosidi/fi-ber-detec-api/raw/main/model_final%20(1).pth"
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
-    response = wget.download(URL, "model_final.pth")
+    os.mkdir("./outputs")
+    response = wget.download(URL, "./outputs/model_final.pth")
     
 
 if __name__ == "__main__":
